0f57cb138f410afa053a62c51cbaab191b973b52
Abson School <absonschool@gmail.com>
2026-05-27 15:07:15 +0300

    Add Google Analytics 4

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -75,18 +76,25 @@ alternates: {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable}`}
-    >
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-        }}
-      >
-        {children}
-      </body>
-    </html>
+    <html lang="en">
+  <body>
+    {children}
+
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+      strategy="afterInteractive"
+    />
+
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-DJW4BZEC8H');
+      `}
+    </Script>
+  </body>
+</html>
   );
 }
